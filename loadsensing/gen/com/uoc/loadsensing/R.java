8e97be0dbd05d9ec09bc822012e248cfde20ae60
Ruben Mendez <ruben.mendez@gmail.com>
2011-11-06 12:56:28 +0100

    - Cambios visuales en la ventana de Login para que tenga un mejor aspecto visual. - He añadido colors.xml para meter perfiles de color para la aplicacion. - He añadido styles.xml para introducir temas a los elementos de la aplicacion.

--- loadsensing/gen/com/uoc/loadsensing/R.java
+++ loadsensing/gen/com/uoc/loadsensing/R.java
@@ -10,6 +10,9 @@ package com.uoc.loadsensing;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int backgroung_app=0x7f040000;
+    }
     public static final class drawable {
         public static final int bg=0x7f020000;
         public static final int ico_barcode_gray=0x7f020001;
@@ -27,29 +30,30 @@ public final class R {
         public static final int icon_barcode_tab=0x7f02000d;
         public static final int icon_globe_tab=0x7f02000e;
         public static final int icon_list_tab=0x7f02000f;
-        public static final int logo=0x7f020010;
-        public static final int repeat_bg=0x7f020011;
-        public static final int tag=0x7f020012;
+        public static final int login_button_theme=0x7f020010;
+        public static final int logo=0x7f020011;
+        public static final int repeat_bg=0x7f020012;
+        public static final int tag=0x7f020013;
     }
     public static final class id {
-        public static final int btnLogin=0x7f050004;
-        public static final int btnScanQR=0x7f05000c;
-        public static final int btnSendQR=0x7f05000d;
-        public static final int descripttext=0x7f050010;
-        public static final int editTextShowLocation=0x7f05000b;
-        public static final int et_pass=0x7f050003;
-        public static final int et_user=0x7f050002;
-        public static final int footer=0x7f050001;
-        public static final int header=0x7f050000;
-        public static final int icon=0x7f05000e;
-        public static final int mapa=0x7f050007;
-        public static final int mapview=0x7f050006;
-        public static final int menu_images=0x7f05000a;
-        public static final int menu_sensors=0x7f050009;
-        public static final int menu_state=0x7f050008;
-        public static final int nametext=0x7f05000f;
-        public static final int network_name=0x7f050011;
-        public static final int visit_worldsensing=0x7f050005;
+        public static final int btnLogin=0x7f070004;
+        public static final int btnScanQR=0x7f07000c;
+        public static final int btnSendQR=0x7f07000d;
+        public static final int descripttext=0x7f070010;
+        public static final int editTextShowLocation=0x7f07000b;
+        public static final int et_pass=0x7f070003;
+        public static final int et_user=0x7f070002;
+        public static final int footer=0x7f070001;
+        public static final int header=0x7f070000;
+        public static final int icon=0x7f07000e;
+        public static final int mapa=0x7f070007;
+        public static final int mapview=0x7f070006;
+        public static final int menu_images=0x7f07000a;
+        public static final int menu_sensors=0x7f070009;
+        public static final int menu_state=0x7f070008;
+        public static final int nametext=0x7f07000f;
+        public static final int network_name=0x7f070011;
+        public static final int visit_worldsensing=0x7f070005;
     }
     public static final class layout {
         public static final int footer_repeat=0x7f030000;
@@ -66,29 +70,32 @@ public final class R {
         public static final int tabmenu=0x7f03000b;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int lst_click_network=0x7f040009;
+        public static final int app_name=0x7f050001;
+        public static final int lst_click_network=0x7f050009;
         /**  Literales para ListNetworkActivity 
          */
-        public static final int lst_not_available=0x7f040006;
-        public static final int lst_retreive_data=0x7f040008;
-        public static final int lst_wait=0x7f040007;
-        public static final int menu_nt_images=0x7f04000c;
-        public static final int menu_nt_sensors=0x7f04000b;
+        public static final int lst_not_available=0x7f050006;
+        public static final int lst_retreive_data=0x7f050008;
+        public static final int lst_wait=0x7f050007;
+        public static final int menu_nt_images=0x7f05000c;
+        public static final int menu_nt_sensors=0x7f05000b;
         /**  Literales para el option menu de una Network 
          */
-        public static final int menu_nt_state=0x7f04000a;
-        public static final int tab_list=0x7f04000e;
+        public static final int menu_nt_state=0x7f05000a;
+        public static final int tab_list=0x7f05000e;
         /**  Literales para el TabMenu 
          */
-        public static final int tab_map=0x7f04000d;
-        public static final int tab_qrcode=0x7f04000f;
-        public static final int uoc_id=0x7f040000;
-        public static final int url_ws=0x7f040004;
+        public static final int tab_map=0x7f05000d;
+        public static final int tab_qrcode=0x7f05000f;
+        public static final int uoc_id=0x7f050000;
+        public static final int url_ws=0x7f050004;
         /**  Literales para Login Activity 
          */
-        public static final int user_name=0x7f040002;
-        public static final int user_pass=0x7f040003;
-        public static final int visit_ws=0x7f040005;
+        public static final int user_name=0x7f050002;
+        public static final int user_pass=0x7f050003;
+        public static final int visit_ws=0x7f050005;
+    }
+    public static final class style {
+        public static final int LoginButton=0x7f060000;
     }
 }
