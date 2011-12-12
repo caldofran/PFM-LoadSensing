2a4186edc6b32451dd27d3c7eeb8a85705faa3b7
Ruben Mendez <ruben.mendez@gmail.com>
2011-12-12 18:10:57 +0100

    Primera prueba para cargar los usuarios desde un string con un JSON mostrando el resultado por pantalla

--- loadsensing/src/com/uoc/loadsensing/SplashActivity.java
+++ loadsensing/src/com/uoc/loadsensing/SplashActivity.java
@@ -1,5 +1,9 @@
 package com.uoc.loadsensing;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.uoc.loadsensing.LoginActivity;
 import com.uoc.loadsensing.R;
 
@@ -13,6 +17,28 @@ public class SplashActivity extends LoadSensingActivity {
 	protected boolean _active = true;
 	protected int _splashTime = 3000; 
 	public Context mContext;
+	private JSONObject jUsersObject;
+	
+	private String jUsersString = "{\"users\":" +
+			"[{\"userName\": \"Ruben\", \"password\": \"1234\"}," +
+			" {\"userName\": \"Jesus\", \"password\": \"4321\"}]}";
+	
+	private void loadJSON() {
+		try {
+			jUsersObject = new JSONObject(jUsersString);
+			JSONArray menuitemArray = jUsersObject.getJSONArray("users");
+			
+			for (int i = 0; i < menuitemArray.length(); i++) {
+				System.out.println(menuitemArray.getJSONObject(i)
+						.getString("userName").toString());
+				System.out.println(menuitemArray.getJSONObject(i).getString(
+						"password").toString());
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public void onCreate(Bundle savedInstanceState) 
 	{
@@ -26,9 +52,10 @@ public class SplashActivity extends LoadSensingActivity {
             @Override
             public void run() {
                 try {
+                	loadJSON();
                     int waited = 0;
                     while(_active && (waited < _splashTime)) {
-                        sleep(100);
+                    	sleep(100);
                         if(_active) {
                             waited += 100;
                         }
