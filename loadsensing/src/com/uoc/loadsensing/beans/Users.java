f81e592e2476e82d6c69ad6014b7674388cd5bf0
Ruben Mendez <ruben.mendez@gmail.com>
2011-12-18 21:22:50 +0100

    Añadida clase usuarios y creada lista de usuarios estatica en la clase principal LoadSensing para poder acceder a ella en cualquier lugar

---
+++ loadsensing/src/com/uoc/loadsensing/beans/Users.java
@@ -0,0 +1,23 @@
+package com.uoc.loadsensing.beans;
+
+public class Users {
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String _name) {
+		this.name = _name;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String _password) {
+		this.password = _password;
+	}
+
+}
