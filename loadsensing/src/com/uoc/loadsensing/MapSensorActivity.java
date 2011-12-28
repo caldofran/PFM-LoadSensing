67c7a8ff209a2fc79637ae582f282cdb6881523b
Ruben Mendez <ruben.mendez@gmail.com>
2011-12-28 21:22:23 +0100

    Minima modificacion

--- loadsensing/src/com/uoc/loadsensing/MapSensorActivity.java
+++ loadsensing/src/com/uoc/loadsensing/MapSensorActivity.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-//import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
